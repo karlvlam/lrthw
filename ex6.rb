b71fb33103ca2c842e2cfe22484dc1574c5c502a
Karl Lam <karl.lam@nxtomo.hk>
2014-12-09 12:42:54 +0800

    ex6 - more comment

--- ex6.rb
+++ ex6.rb
@@ -1,12 +1,16 @@
 types_of_people = 10
+
+# concat int into a sentence
 x = "There are #{types_of_people} types of people."
 binary = "binary"
 do_not = "don't"
+# concat strings into a sentence
 y = "Those who know #{binary} and those who #{do_not}."
 
 puts x
 puts y
 
+# concat concated strings into a sentence
 puts "I said: #{x}."
 puts "I also said: '#{y}'."
 
@@ -18,4 +22,5 @@ puts joke_evaluation
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# another way to concat strings
 puts w + e
