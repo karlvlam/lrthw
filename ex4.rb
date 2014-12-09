7f602ff61bd10ee6f8dc92bbf5e6911ba44ad0df
Karl Lam <karl.lam@nxtomo.hk>
2014-12-09 11:51:02 +0800

    ex4 with comments

--- ex4.rb
+++ ex4.rb
@@ -1,12 +1,12 @@
 
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
-cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+cars = 100 # number of car
+space_in_a_car = 4.0 # space of each car
+drivers = 30 # number of car dirver
+passengers = 90 # number of passengers
+cars_not_driven = cars - drivers # number of cars without driver
+cars_driven = drivers # number of cars 
+carpool_capacity = cars_driven * space_in_a_car # capacity of driven cars
+average_passengers_per_car = passengers / cars_driven # passenger per car
 
 puts "There are #{cars} cars avilable."
 puts "There are only #{drivers} drivers avilable."
