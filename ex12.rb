de10c4f2c8ee70d54394c7e15c069dd9059fdc33
Karl Lam <karl.lam@nxtomo.hk>
2014-12-10 15:17:52 +0800

    ex12 - use .to_f and change money 10% script

--- ex12.rb
+++ ex12.rb
@@ -1,12 +1,12 @@
 print "Give me a number: "
-number = gets.chomp.to_i
+number = gets.chomp.to_f
 
 bigger = number * 100
 puts "A bigger number is #{bigger}."
 
 print "give me another number: "
 another = gets.chomp
-number = another.to_i
+number = another.to_f
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
---
+++ ex12b.rb
@@ -0,0 +1,7 @@
+print "Give me some money: "
+money = gets.chomp.to_f
+
+change = money * 0.1 
+puts "Here's the change #{change}."
+
+
