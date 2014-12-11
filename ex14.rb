e4f161de5ea493f642b95af712314b391861c20d
Karl Lam <karl.lam@nxtomo.hk>
2014-12-11 17:07:00 +0800

    ex14 - Study Drills

--- ex14.rb
+++ ex14.rb
@@ -1,19 +1,19 @@
-user_name = ARGV.first
-prompt = '> '
+user_name, gender = ARGV
+hints = '> '
 
 puts "Hi #{user_name}."
 puts "I'd like to ask you a few questions."
-puts "Do you like me #{user_name}? ", prompt
+puts "Do you like me #{user_name}, #{gender} ? ", hints
 likes = $stdin.gets.chomp
 
-puts "Where do you live #{user_name}? ", prompt
+puts "Where do you live #{user_name}? ", hints
 lives = $stdin.gets.chomp
 
-puts "What kind of computer do you have? ", prompt
+puts "What kind of computer do you have? ", hints
 computer = $stdin.gets.chomp
 
 puts """
-Alright, so you said #{likes} about liking me.
+Alright, #{gender} so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
 and you have a #{computer} computer. Nice.
 """
