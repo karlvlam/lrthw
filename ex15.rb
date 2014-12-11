d3589e9738aa52ddbd77dbe230473fa3628fdfd9
Karl Lam <karl.lam@nxtomo.hk>
2014-12-11 17:27:47 +0800

    ex15 - with comment

--- ex15.rb
+++ ex15.rb
@@ -1,13 +1,13 @@
-filename = ARGV.first
+filename = ARGV.first # get file name
 
-txt = open(filename)
+txt = open(filename) # open file, return object
 
 puts "Here's your file #{filename}:"
-print txt.read
+print txt.read # print the content of txt
 
 print "Type the filename again: "
 file_again = $stdin.gets.chomp
 
-txt_again = open(file_again)
+txt_again = open(file_again) # another open file object
 
 print txt_again.read
